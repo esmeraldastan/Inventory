f27a319f6a40cc3d94cc076e837fdbaf05e3ddd2
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-03 17:27:03 -0700

    changes

--- Inventory.py
+++ Inventory.py
@@ -1,4 +1,4 @@
-#INVENTORY FOR ITEMS/WEAPONS
+#INVENTORY FOR ITEMS
 inventory = []
 
 def addToInventory(item):
@@ -12,3 +12,30 @@ else:
     print ("")
 print (inventory)
 
+#INVENTORY FOR WEAPONS
+
+class player(object):
+    
+    def __init__(self, name, max_items, item):
+        self.name = name
+        self.max_items = max_items
+        self.item = item
+        
+    def inventory(self):
+        for item in self.items:
+            print item
+            
+    def take(self, new_item):
+        if len(self.items)<self.max_items:
+            self.items.append(new_item)
+        else: 
+            print "You have reach a weight limit.\nYou can't carry anymore weapons"
+            
+    def leave(self, old_item):
+        if old_item in self.items:
+            self.items.remove(old_item)
+        else:
+            print "Not found."
+        
+            
+        
